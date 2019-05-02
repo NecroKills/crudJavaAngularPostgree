afb977d33685c596f4630b2f947df719e19fb873
maycon da silva moreira <maycondasilvamoreira@hotmail.com>
2019-05-02 17:16:06 -0300

    subindo back-end, java spring com postgree. Front-end com angular

---
+++ front-angular/src/app/pais.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { Pais } from 'src/app/pais';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+
+@Injectable()
+export class PaisService {
+
+  url = "http://localhost:8080/api/pais";
+
+  constructor(private http: HttpClient) {
+  }
+
+  findAll(): Observable<Pais[]>{
+    return this.http.get<Pais[]>(`${this.url}`);
+  }
+
+  findById(id: number): Observable<Pais[]> {
+    return this.http.get<Pais[]>(`${this.url}/${id}`);
+
+
+  }
+  save(pais: Pais){
+    return this.http.post(`${this.url}`, JSON.stringify(pais), httpOptions);
+  }
+
+  delete(id: number){
+      return this.http.delete(`${this.url}/${id}`);
+  }
+
+
+}
